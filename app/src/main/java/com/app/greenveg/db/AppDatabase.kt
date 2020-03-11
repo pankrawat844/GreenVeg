9a01373e02c194a8d591b851335fd9d13f43724d
Sumit <sumitjangir554@gmail.com>
2020-03-11 18:04:37 +0530

    Add Room database

---
+++ app/src/main/java/com/app/greenveg/db/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.app.greenveg.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(),version = 1)
+abstract class AppDatabase:RoomDatabase() {
+
+}
