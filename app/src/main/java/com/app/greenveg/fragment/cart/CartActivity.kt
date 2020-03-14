c314fcbeced31f6ff57f1a70a6cba9b6e75ff96b
pankaj <pankrawat9@gmail.com>
2020-03-14 08:27:11 +0530

    Add to cart

---
+++ app/src/main/java/com/app/greenveg/fragment/cart/CartActivity.kt
@@ -0,0 +1,12 @@
+package com.app.greenveg.fragment.cart
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.greenveg.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
